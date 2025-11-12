6a795c56dc9322026b64bddd06829b8183ee72ec
FYoldash <faridoon.yoldash@gmail.com>
2025-11-12 12:58:43 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const navbar = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    navbar.classList.toggle('menu-active'); // toggles white color
+});
+
+
+
